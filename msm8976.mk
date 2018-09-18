405b8e1e4e5d605804861b49ca737a3858d824a2
Ramjee Singh <ramjee@codeaurora.org>
2018-09-18 12:40:44 +0530

    msm8976-common: ST HAL upgrade for P compatibility
    -Sound trigger HIDL interface supports loading of more
    than 1MB sound models from 2.1 version. Update to use HIDL
    version 2.1.
    
    Change-Id: I564f598f41f20a01ba9e8dd69be125097a134f1c
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- msm8976.mk
+++ msm8976.mk
@@ -80,7 +80,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
